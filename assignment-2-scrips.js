f78d9798d7a17e3c260d5f946ef21071315ed4f8
mirandahazoury08 <mh5120@nyu.edu>
2024-04-11 17:05:02 -0400

    Port jsfiddle into html,css, jscript

---
+++ assignment-2-scrips.js
@@ -0,0 +1,13 @@
+$('.box1').on('click', function() {
+    $('body').css('background-color', '#AEB06F')
+  })
+  
+  $('.box2').on('click', function() {
+    $('body').css('background-color', '#A6ABAA')
+  })
+  
+  $('.box3').on('click', function() {
+    $('body').css('background-color', '#970D0D')
+  })
+  
+  
