ca60b06f157098bb0f1205ae07409b126b8fe20e
mirandahazoury08 <mh5120@nyu.edu>
2024-05-02 16:08:25 -0400

    Edited Original--Background Image
    Editing Past Jsfiddle

--- assignment-2-scrips.js
+++ assignment-2-scrips.js
@@ -1,13 +1,20 @@
-$('.box1').on('click', function() {
-    $('body').css('background-color', '#AEB06F')
-  })
+$(document).ready(function() {
+  $(".box1").click(function() {
+    $("body").css("background-image", 'url("https://th.bing.com/th/id/OIP.nDxt46lPiHoieWjDUxeFtgHaEK?w=329&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7")');
+  });
+});
+
+$(document).ready(function() {
+  $(".box2").click(function() {
+    $("body").css("background-image", 'url("https://i.pinimg.com/564x/8e/f5/5b/8ef55bcc9fd87d038004865cb431fdd5.jpg")');
+  });
+});
+
+$(document).ready(function() {
+  $(".box3").click(function() {
+    $("body").css("background-image", 'url("https://i.pinimg.com/564x/ea/76/6d/ea766d668a03a89ed43bc830fb64c0fa.jpg")');
+  });
+});
   
-  $('.box2').on('click', function() {
-    $('body').css('background-color', '#A6ABAA')
-  })
-  
-  $('.box3').on('click', function() {
-    $('body').css('background-color', '#970D0D')
-  })
-  
-  
+
+ 
